7cb4fe46686e84ef9e40762604046f4f5fbe6d97
Majd Alshamali <majdalshamali@Majds-MacBook-Pro.local>
2022-06-28 12:57:06 +0300

    third push

--- database/migrations/2022_06_28_015837_create_posts_table.php
+++ database/migrations/2022_06_28_015837_create_posts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('title');
             $table->mediumText('body');
             $table->timestamps();
+            
         });
     }
 
